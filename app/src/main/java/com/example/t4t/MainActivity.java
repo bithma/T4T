8b582f4474aca614fd3ee747dc7f4b5d13c196cf
Russell Davidson <103starwars@gmail.com>
2023-01-14 15:45:29 -0500

    Stuff

--- app/src/main/java/com/example/t4t/MainActivity.java
+++ app/src/main/java/com/example/t4t/MainActivity.java
@@ -20,7 +20,6 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.t4t.databinding.ActivityMainBinding;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
